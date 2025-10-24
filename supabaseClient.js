dd6ce46227906c808a833238535fb1c12b862044
Hipolito <167230845+bielhipolito@users.noreply.github.com>
2025-10-24 13:07:05 -0300

    Update Supabase client initialization method
    atua.

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,9 +5,10 @@
 
 const SUPABASE_URL = 'https://kbaltnbemvxsijntsqgf.supabase.co';
 // SUBSTITUA O VALOR ABAIXO PELA SUA CHAVE COMPLETA
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiYWx0bmJlbXZ4c2lqbnRzcWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU0MjM0NTYsImV4cCI6MTgyMTA3OTQ1Nn0.VABWaR_image.png'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiYWx0bmJlbXZ4c2lqbnRzcWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NTk1NTcsImV4cCI6MjA3NjIzNTU1N30._R6FSMckCbwH5rem-nKYJ3NcTzGL6e14j3iMU_sx8M0'; 
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY );
+// Correção: Usa window.supabase.createClient exposto pela CDN
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY  );
 
 // Exporta o cliente Supabase para ser usado em outros arquivos JS
 export { supabase };
